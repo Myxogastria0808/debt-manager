5d49b4783915aa496e7cda7cb8b8c3099ab8a245
Redaction-F <koebijakobiteorhang@gmail.com>
2025-09-18 14:12:33 +0900

    fix: make backend be accessed from frontend

--- products/backend/src/index.ts
+++ products/backend/src/index.ts
@@ -2,12 +2,23 @@ import { drizzle } from 'drizzle-orm/d1';
 import { Hono } from 'hono';
 import { users } from './db/schema';
 import { user } from './handler/route/script';
+import { cors } from 'hono/cors';
 
 export type Bindings = {
   DB: D1Database;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
+
+app.use(
+  '/*',
+  cors({
+    origin: ['*'],
+    allowHeaders: ['Content-Type'],
+    allowMethods: ['*'],
+    exposeHeaders: ['Content-Type'],
+  })
+);
 
 app.route('/', user);
 
