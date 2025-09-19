57b475a1cf4c2b0a451ebe802d849550bc01514c
Redaction-F <koebijakobiteorhang@gmail.com>
2025-09-19 10:53:32 +0900

    chore: backend delete api

--- products/backend/src/infrastructure/historys.ts
+++ products/backend/src/infrastructure/historys.ts
@@ -1,6 +1,7 @@
 import { drizzle, DrizzleD1Database } from 'drizzle-orm/d1';
 import { historys } from '../db/schema';
 import { Bindings } from '../domain/script';
+import { eq } from 'drizzle-orm';
 
 export const insertHistoryDB = async (
   env: Bindings,
@@ -19,4 +20,10 @@ export const selectHistoryDBAll = async (env: Bindings) => {
   const db = drizzle(env.DB);
 
   return await db.select().from(historys).all();
+};
+
+export const deleteHistoryDBById = async (env: Bindings, id: number) => {
+  const db = drizzle(env.DB);
+
+  return await db.delete(historys).where(eq(historys.id, id));
 };
