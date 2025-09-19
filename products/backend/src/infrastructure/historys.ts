ef40077ced238faca7cfbd6112d53f30fc751f8e
h-yki <yuuki531259@gmail.com>
2025-09-19 13:35:56 +0900

    履歴の合算・相殺を追加

--- products/backend/src/infrastructure/historys.ts
+++ products/backend/src/infrastructure/historys.ts
@@ -1,7 +1,8 @@
 import { drizzle, DrizzleD1Database } from 'drizzle-orm/d1';
 import { historys } from '../db/schema';
 import { Bindings } from '../domain/script';
-import { eq } from 'drizzle-orm';
+import { eq, and } from 'drizzle-orm';
+import { env } from 'cloudflare:workers';
 
 export const insertHistoryDB = async (
   env: Bindings,
@@ -26,4 +27,14 @@ export const deleteHistoryDBById = async (env: Bindings, id: number) => {
   const db = drizzle(env.DB);
 
   return await db.delete(historys).where(eq(historys.id, id));
+};
+
+export const selectHistoryDBByFromTo = async (env: Bindings, from: string, to: string) => {
+  const db = drizzle(env.DB);
+
+  return await db
+    .select()
+    .from(historys)
+    .where(and(eq(historys.from, from), eq(historys.to, to)))
+    .all();
 };
